e73be84907f1c18c30cf0e3c8d549e9442372b0e
hoanjp197 <hoanjp197@gmail.com>
2024-05-31 02:14:22 +0700

    sidebar

---
+++ source/resources/js/Layouts/Sidebar.vue
@@ -0,0 +1,123 @@
+<template>
+  <a-layout style="min-height: 100vh">
+    <a-layout-sider v-model:collapsed="collapsed" collapsible>
+      <div class="logo" />
+      <a-menu v-model:selectedKeys="selectedKeys" theme="light" mode="inline">
+        <a-menu-item key="1">
+          <pie-chart-outlined />
+          <span>Option 1</span>
+        </a-menu-item>
+        <a-menu-item key="2">
+          <desktop-outlined />
+          <span>Option 2</span>
+        </a-menu-item>
+        <a-sub-menu key="sub1">
+          <template #title>
+            <span>
+              <user-outlined />
+              <span>User</span>
+            </span>
+          </template>
+          <a-menu-item key="3">Tom</a-menu-item>
+          <a-menu-item key="4">Bill</a-menu-item>
+          <a-menu-item key="5">Alex</a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub2">
+          <template #title>
+            <span>
+              <team-outlined />
+              <span>Team</span>
+            </span>
+          </template>
+          <a-menu-item key="6">Team 1</a-menu-item>
+          <a-menu-item key="8">Team 2</a-menu-item>
+        </a-sub-menu>
+        <a-menu-item key="9">
+          <file-outlined />
+          <span>File</span>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header style="background: #fff; padding: 0" />
+      <a-layout-content style="margin: 0 16px">
+        <a-breadcrumb style="margin: 16px 0">
+          <a-breadcrumb-item>User</a-breadcrumb-item>
+          <a-breadcrumb-item>Bill</a-breadcrumb-item>
+        </a-breadcrumb>
+        <div :style="{ padding: '24px', background: '#fff', minHeight: '360px' }">
+          Bill is a cat.
+        </div>
+      </a-layout-content>
+      <a-layout-footer style="text-align: center">
+        Ant Design ©2018 Created by Ant UED
+      </a-layout-footer>
+    </a-layout>
+  </a-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue';
+import { PieChartOutlined, DesktopOutlined, UserOutlined, TeamOutlined, FileOutlined } from '@ant-design/icons-vue';
+import { Layout, Menu, Breadcrumb } from 'ant-design-vue';
+
+export default defineComponent({
+  components: {
+    PieChartOutlined,
+    DesktopOutlined,
+    UserOutlined,
+    TeamOutlined,
+    FileOutlined,
+    'a-layout': Layout,
+    'a-layout-sider': Layout.Sider,
+    'a-layout-header': Layout.Header,
+    'a-layout-content': Layout.Content,
+    'a-layout-footer': Layout.Footer,
+    'a-menu': Menu,
+    'a-menu-item': Menu.Item,
+    'a-sub-menu': Menu.SubMenu,
+    'a-breadcrumb': Breadcrumb,
+    'a-breadcrumb-item': Breadcrumb.Item,
+  },
+  setup() {
+    const collapsed = ref(false);
+    const selectedKeys = ref(['1']);
+    
+    return {
+      collapsed,
+      selectedKeys,
+    };
+  },
+});
+</script>
+
+<style>
+#components-layout-demo-side .logo {
+  height: 32px;
+  margin: 16px;
+  background: rgba(255, 255, 255, 0.3);
+}
+
+.site-layout .site-layout-background {
+  background: #fff;
+}
+[data-theme='dark'] .site-layout .site-layout-background {
+  background: #141414;
+}
+
+.ant-layout-sider {
+  background-color: #fff !important; /* Set the background color to white and override other styles */
+}
+
+.ant-menu-dark, .ant-menu-dark .ant-menu-sub {
+  background-color: #fff !important; /* Set the menu background color to white */
+}
+
+.ant-menu-item-selected {
+  background-color: rgba(0, 0, 0, 0.04) !important; /* Set the selected item background color */
+}
+
+.ant-menu-item {
+  color: rgba(0, 0, 0, 0.85) !important; /* Set the menu item text color */
+}
+</style>
